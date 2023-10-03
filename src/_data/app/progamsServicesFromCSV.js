f36c5fd6a866f7d6d84657e21b39f50c400c529d
Akezhan Kaliyev <Akezhan.Kaliyev@ontario.ca>
2023-10-03 13:50:17 -0400

    updated csv file added

--- src/_data/app/progamsServicesFromCSV.js
+++ src/_data/app/progamsServicesFromCSV.js
@@ -8,8 +8,8 @@ module.exports = async function(language = 'en') {
   // Determine the file path based on the language
   const filePath = language === 'fr'
     ? `${__dirname}/../../assets/csv/French New Seniors Content June 2023.csv`
-    : `${__dirname}/../../assets/csv/English Seniors Content June 2023.csv`;
-
+    //: `${__dirname}/../../assets/csv/English Seniors Content June 2023.csv`;
+    : `${__dirname}/../../assets/csv/Copy of New seniors portal secondary serach content Sept 2023 JCedits_v2_CO.csv`;
   // Read and parse the CSV file
   const file = fs.readFileSync(filePath, 'utf-8');
   let seniorsContent = Papa.parse(stripBom(file), {
